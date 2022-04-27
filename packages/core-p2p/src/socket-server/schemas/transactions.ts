3602a648d88c229033162add5a65844662ba2d4a
tnga <devtnga@gmail.com>
2022-04-27 15:14:25 +0100

    add `packages/core-p2p` from code base

---
+++ packages/core-p2p/src/socket-server/schemas/transactions.ts
@@ -0,0 +1,22 @@
+import { Container, Contracts, Providers } from "@cauriland/core-kernel";
+import Joi from "joi";
+
+import { headers } from "./shared";
+
+export const transactionsSchemas = {
+    createPostTransactionsSchema: (app: Contracts.Kernel.Application): Joi.AnySchema =>
+        Joi.object({
+            transactions: Joi.array()
+                .items(Joi.binary())
+                .max(
+                    app
+                        .getTagged<Providers.PluginConfiguration>(
+                            Container.Identifiers.PluginConfiguration,
+                            "plugin",
+                            "@cauriland/core-transaction-pool",
+                        )
+                        .getOptional<number>("maxTransactionsPerRequest", 40),
+                ),
+            headers,
+        }),
+};
---
+++ packages/core-p2p/src/socket-server/utils/validate.ts
@@ -0,0 +1,14 @@
+import { Validation } from "@cauriland/crypto";
+
+import { SocketErrors } from "../../enums";
+
+export const validate = (schema, data) => {
+    const { error: validationError } = Validation.validator.validate(schema, data);
+
+    if (validationError) {
+        const error = new Error(`Data validation error : ${validationError}`);
+        error.name = SocketErrors.Validation;
+
+        throw error;
+    }
+};
